29879f8abb6aa7668d34aeca18808275f897bc89
casperlundberg <cappe5@hotmail.se>
2022-10-10 13:02:08 +0200

    svg loading error

--- src/App.js
+++ src/App.js
@@ -24,8 +24,7 @@
 import React from "react";
 import "./App.css";
 
-// eslint-disable-next-line no-unused-vars
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 
 import BlocklyComponent, {
   Block,
