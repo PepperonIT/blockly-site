0c15525c1cfc836f8d3a89e216e6d73fbd482fb7
casperlundberg <cappe5@hotmail.se>
2022-11-17 13:45:14 +0100

    speech fix

--- src/generator/speechGen.js
+++ src/generator/speechGen.js
@@ -5,5 +5,5 @@ import "blockly/python";
 
 Blockly.Python["say"] = function (block) {
     var text = block.getFieldValue("text");
-    return `pep_speech.talk(${String(text)})\n`;
+    return `pep_speech.talk("${String(text)}")\n`;
 };
