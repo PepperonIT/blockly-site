e0a554187f8ae7824072bd3d8ee2baa62e58cdcf
casperlundberg <cappe5@hotmail.se>
2022-11-17 10:47:55 +0100

    cleraification

--- src/blockly/BlocklyComponent.jsx
+++ src/blockly/BlocklyComponent.jsx
@@ -31,6 +31,7 @@ import locale from "blockly/msg/sv";
 import "blockly/blocks";
 import axios from "axios";
 
+// Get yout IP on Linux by running `ifconfig`
 const myIP = "130.240.156.237" // Only use localhost if site is running on dev-machine ONLY. Otherwise use full IP! Remeber to open ports!
 
 Blockly.setLocale(locale);
