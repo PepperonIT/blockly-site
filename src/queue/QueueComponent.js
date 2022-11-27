4ae514fcf6bfb608384dba893538ce318b50254c
SimonHotDog <simon.lu@live.se>
2022-11-27 20:52:12 +0100

    Queue is now updated every second with placeholder numbers

--- src/queue/QueueComponent.js
+++ src/queue/QueueComponent.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import { Value } from "../blockly";
 import "./QueueComponent.css";
 
 class QueueComponent extends Component {
-  state = {
-    queue_items: ["List Item 1", "List Item 2", "List Item 3"]
-  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      testDigit: [1, 2, 3]
+    }
+  }
+
+  componentDidMount = () => {
+    this.myTimer = setInterval(() => {
+      this.setState((prevState) => ({
+        testDigit: prevState.testDigit.map(value => value + 1) //Testing placeholder
+      }));
+    }, 1000) //Update interval
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.myTimer);
+  }
+
+  //state = {
+    //queue_items: ["List Item 1", "List Item 2", "List Item 3"]
+  //};
 
   render() {
     return (
@@ -14,7 +35,7 @@ class QueueComponent extends Component {
             Kö
             <hr class="solid"></hr>
             </h1></li>
-            {this.state.queue_items.map(listitem => (
+            {this.state.testDigit.map(listitem => (
               <li>
                  <span class="b">
                  {listitem}
